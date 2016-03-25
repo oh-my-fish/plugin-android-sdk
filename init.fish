82941432af73638c5723c91bc33ac9414e99738b
Justin Hileman <justin@justinhileman.info>
2016-03-25 00:09:12 -0700

    Only add missing paths during init.
    This prevents issues when reloading the shell. See oh-my-fish/plugin-composer#7

--- init.fish
+++ init.fish
@@ -1,3 +1,9 @@
 set -q ANDROID_SDK_ROOT; or set -l ANDROID_SDK_ROOT /opt/android-sdk
 
-set PATH $ANDROID_SDK_ROOT/platform-tools $ANDROID_SDK_ROOT/tools $PATH
+if not contains $ANDROID_SDK_ROOT/tools $PATH
+  set PATH $ANDROID_SDK_ROOT/tools $PATH
+end
+
+if not contains $ANDROID_SDK_ROOT/platform-tools $PATH
+  set PATH $ANDROID_SDK_ROOT/platform-tools $PATH
+end
