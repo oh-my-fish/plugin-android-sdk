c34c3fd2a94163b0f9ee3999d29cd08ff57c5401
Michael Coffey <micof@microsoft.com>
2021-08-03 19:13:39 +0100

    Update for fish 3.x and add more/current cmd directories

--- conf.d/android-sdk.fish
+++ conf.d/android-sdk.fish
@@ -1,13 +1,18 @@
-set paths $ANDROID_HOME/{tool?,platform-tool?}
-  or set paths $ANDROID_SDK_ROOT/{tool?,platform-tool?}
-  or set paths $HOME/Android/Sdk/{tool?,platform-tool?}
-
-if test (count $paths) -gt 0
-  set -l IFS /
-  echo $paths[1] | read -la components
-  set -e components[-1]
-  printf '/%s' $components | read -l root
-
-  set -gx PATH $PATH $paths
-  set -gx ANDROID_HOME $root
+# check for common Android SDK locations
+for root in $ANDROID_SDK_ROOT $ANDROID_SDK_ROOT $HOME/Android/Sdk $HOME/Library/Android/sdk
+  set -l paths
+  if test -d $root
+    # the SDK root exists as a directory so let's try to add the binary directories
+    for bin in $root/cmdline-tools/latest/bin "$root/build-tools/"(ls $root/build-tools)[-1] $root/platform-tools $root/emulator 
+      if test -d $bin
+        set paths $paths $bin
+      end
+    end
+    # if we added at least one path, let's add them to the real PATH set ANDROID_HOME and finish
+    if test (count $paths) -gt 0
+      set -gx PATH $PATH $paths
+      set -gx ANDROID_HOME $root
+      break
+    end
+   end
 end
